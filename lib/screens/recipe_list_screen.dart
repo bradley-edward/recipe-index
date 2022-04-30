1dec537c6177f865957e18d616e6975c4416729b
MapleJupiter <sunflower-beacon@protonmail.com>
2022-04-30 14:58:08 +0800

    Extract the ListView displaying recipe entries into its own Widget.

--- lib/screens/recipe_list_screen.dart
+++ lib/screens/recipe_list_screen.dart
@@ -1,41 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/recipe_collection.dart';
-import './recipe_details_screen.dart';
+import '../widgets/collection_list.dart';
 
 class RecipeListScreen extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
-		var dummyData = Provider.of<RecipeCollection>(context).entries;
-
 		return Scaffold(
 			appBar: AppBar(
-				title: const Text("Recipe List"),
+				title: const Text("Recipes"),
 			),
 			body: Container(
 				width: double.infinity,
 				height: 400,
-				child: ListView.builder(
-					itemCount: dummyData.length,
-					itemBuilder: (ctx, index) {
-						var currEntry = dummyData[index];
-						return ListTile(
-							leading: Container(
-								width: 50,
-								height: 50,
-								decoration: const BoxDecoration(
-									color: Colors.red,
-								),
-							),
-							title: Text(currEntry.name),
-							subtitle: Text(currEntry.entryId),
-							onTap: () {
-								Navigator.of(context).pushNamed(RecipeDetailsScreen.routeName, arguments: currEntry.id);
-							},
-						);
-					}
-				),
+				child: CollectionList(),
 			),
 		);
 	}
---
+++ lib/widgets/collection_list.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/recipe_collection.dart';
+import '../screens/recipe_details_screen.dart';
+
+class CollectionList extends StatelessWidget {
+	const CollectionList({ Key? key }) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) {
+		var dummyData = Provider.of<RecipeCollection>(context).entries;
+
+		return ListView.builder(
+			itemCount: dummyData.length,
+			itemBuilder: (ctx, index) {
+				var currEntry = dummyData[index];
+				return ListTile(
+					leading: Container(
+						width: 50,
+						height: 50,
+						decoration: const BoxDecoration(
+							color: Colors.red,
+						),
+					),
+					title: Text(currEntry.name),
+					subtitle: Text(currEntry.entryId),
+					onTap: () {
+						Navigator.of(context).pushNamed(RecipeDetailsScreen.routeName, arguments: currEntry.id);
+					},
+				);
+			}
+		);
+	}
+}
