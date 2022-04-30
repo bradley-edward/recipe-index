56f99fe87e2260fa5346c85595b7acf22486eb13
MapleJupiter <sunflower-beacon@protonmail.com>
2022-04-30 12:01:00 +0800

    Extract the dummy data to its own file, in 'models'.

---
+++ lib/models/dummy_data.dart
@@ -0,0 +1,22 @@
+final dummyData = <Map<String,dynamic>>[
+	{
+		'id': 'e1',
+		'entryId': '0001',
+		'name': 'Glazed Carrots'
+	},
+	{
+		'id': 'e2',
+		'entryId': '0002',
+		'name': 'Brown Sugar Mustard Glazed Ham'
+	},
+	{
+		'id': 'e3',
+		'entryId': '0004',
+		'name': 'Philly Cheesesteak'
+	},
+	{
+		'id': 'e4',
+		'entryId': '0006',
+		'name': 'Soy-Balsamic Glazed Sea Scallops'
+	},
+];
--- lib/screens/recipe_list_screen.dart
+++ lib/screens/recipe_list_screen.dart
@@ -1,27 +1,6 @@
 import 'package:flutter/material.dart';
 
-final dummyData = <Map<String,dynamic>>[
-	{
-		'id': 'e1',
-		'entryId': '0001',
-		'name': 'Glazed Carrots'
-	},
-	{
-		'id': 'e2',
-		'entryId': '0002',
-		'name': 'Brown Sugar Mustard Glazed Ham'
-	},
-	{
-		'id': 'e3',
-		'entryId': '0004',
-		'name': 'Philly Cheesesteak'
-	},
-	{
-		'id': 'e4',
-		'entryId': '0006',
-		'name': 'Soy-Balsamic Glazed Sea Scallops'
-	},
-];
+import '../models/dummy_data.dart';
 
 class RecipeListScreen extends StatelessWidget {
 	@override
