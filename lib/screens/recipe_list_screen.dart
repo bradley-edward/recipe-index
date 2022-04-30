f8183f87924544b4e79bda0963729455acbdae0a
MapleJupiter <sunflower-beacon@protonmail.com>
2022-04-30 11:38:41 +0800

    Start work on displaying items in a collection.

--- lib/screens/recipe_list_screen.dart
+++ lib/screens/recipe_list_screen.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
+final dummyData = <Map<String,dynamic>>[
+	{
+		'id': 'e1',
+		'entryId': '0001',
+		'name': 'Glazed Carrots'
+	},
+	{
+		'id': 'e2',
+		'entryId': '0002',
+		'name': 'Brown Sugar Mustard Glazed Ham'
+	},
+	{
+		'id': 'e3',
+		'entryId': '0004',
+		'name': 'Philly Cheesesteak'
+	},
+	{
+		'id': 'e4',
+		'entryId': '0006',
+		'name': 'Soy-Balsamic Glazed Sea Scallops'
+	},
+];
+
 class RecipeListScreen extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
@@ -7,8 +30,26 @@ class RecipeListScreen extends StatelessWidget {
 			appBar: AppBar(
 				title: const Text("Recipe List"),
 			),
-			body: const Center(
-				child: Text("Text goes here"),
+			body: Container(
+				width: double.infinity,
+				height: 400,
+				child: ListView.builder(
+					itemCount: dummyData.length,
+					itemBuilder: (ctx, index) {
+						var currEntry = dummyData[index];
+						return ListTile(
+							leading: Container(
+								width: 50,
+								height: 50,
+								decoration: const BoxDecoration(
+									color: Colors.red,
+								),
+							),
+							title: Text(currEntry['name']),
+							subtitle: Text(currEntry['entryId']),
+						);
+					}
+				),
 			),
 		);
 	}
