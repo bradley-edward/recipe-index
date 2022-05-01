d65178105074ab556a167abe2f41fa970f4a3b17
MapleJupiter <sunflower-beacon@protonmail.com>
2022-05-01 18:58:32 +0800

    Recipe Details screen should listen for changes to the entryList provider.

--- lib/screens/recipe_details_screen.dart
+++ lib/screens/recipe_details_screen.dart
@@ -10,7 +10,7 @@ class RecipeDetailsScreen extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		var entryId = ModalRoute.of(context)!.settings.arguments as String;
-		var entry = Provider.of<RecipeCollection>(context, listen: false).findById(entryId);
+		var entry = Provider.of<RecipeCollection>(context).findById(entryId);
 
 		return Scaffold(
 			appBar: AppBar(
