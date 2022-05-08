acdc78c82ce2270ae976c40798d43febd8e62050
MapleJupiter <sunflower-beacon@protonmail.com>
2022-05-08 20:17:50 +0800

    Make Recipe Details screen listen to changes to provider.

--- lib/screens/recipe_details_screen.dart
+++ lib/screens/recipe_details_screen.dart
@@ -41,7 +41,7 @@ class RecipeDetailsScreen extends StatelessWidget {
 		final appNav = Navigator.of(context);
 
 		var entryId = ModalRoute.of(context)!.settings.arguments as String;
-		var collectionProvider = Provider.of<RecipeCollection>(context, listen: false);
+		var collectionProvider = Provider.of<RecipeCollection>(context);
 		final entry = collectionProvider.findById(entryId);
 
 		return Scaffold(
