740c167ef09f4b217dbd60fd1ac5503029303e80
MapleJupiter <sunflower-beacon@protonmail.com>
2022-05-11 21:46:26 +0800

    Improve the dummy data, so as to use something that can quickly populate the DB, for future development.

--- lib/models/dummy_data.dart
+++ lib/models/dummy_data.dart
@@ -1,22 +1,44 @@
-final dummyData = <Map<String,dynamic>>[
-	{
-		'id': 'e1',
-		'entryId': '0001',
-		'name': 'Glazed Carrots'
-	},
-	{
-		'id': 'e2',
-		'entryId': '0002',
-		'name': 'Brown Sugar Mustard Glazed Ham'
-	},
-	{
-		'id': 'e3',
-		'entryId': '0004',
-		'name': 'Philly Cheesesteak'
-	},
-	{
-		'id': 'e4',
-		'entryId': '0006',
-		'name': 'Soy-Balsamic Glazed Sea Scallops'
-	},
+import './recipe_entry.dart';
+import './recipe_complexity.dart';
+import './technical_difficulty.dart';
+import './entry_image.dart';
+
+final dummyData = <RecipeEntry>[
+	RecipeEntry(
+		id: null,
+		entryId: '0001',
+		name: 'Glazed Carrots',
+		difficulty: TechnicalDifficulty.easy,
+		complexity: RecipeComplexity.moderate,
+		images: [
+			EntryImage(
+				id: null,
+				imageLocation: 'https://www.recipetineats.com/wp-content/uploads/2018/11/Brown-Sugar-Roast-Carrots_3.jpg',
+				imageType: ImageType.fromInternet,
+			),
+			EntryImage(
+				id: null,
+				imageLocation: 'https://www.recipetineats.com/wp-content/uploads/2018/11/Brown-Sugar-Roast-carrots.jpg',
+				imageType: ImageType.fromInternet,
+			),
+		]
+	),
+	RecipeEntry(
+		id: null,
+		entryId: '0002',
+		name: 'Sweetcorn Soup',
+		difficulty: TechnicalDifficulty.easy,
+		complexity: RecipeComplexity.moderate,
+		images: [
+			EntryImage(
+				id: null,
+				imageLocation: 'https://meaningfuleats.com/wp-content/uploads/2017/07/corn-soup.jpg',
+				imageType: ImageType.fromInternet,
+			),
+			EntryImage(
+				imageLocation: 'https://meaningfuleats.com/wp-content/uploads/2017/07/sweet-corn-soup.jpg',
+				imageType: ImageType.fromInternet
+			),
+		]
+	),
 ];
