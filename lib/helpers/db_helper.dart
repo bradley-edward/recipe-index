33103c465d7f2539f55929987b31df3a2b588c6d
MapleJupiter <sunflower-beacon@protonmail.com>
2022-08-07 14:39:40 +0800

    Remove all foreign key constraints in general, causing the export and import[overwrite all data] to work in full.

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -13,13 +13,15 @@ class DBHelper {
 			path.join(dbPath, 'collection_indexer.db'),
 			onCreate: (db, version) async {
 				await db.execute('CREATE TABLE recipes(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, displayId TEXT, name TEXT, complexity INTEGER, difficulty INTEGER, prepTime INTEGER, cookingTime INTEGER, servings INTEGER)');
-				await db.execute('CREATE TABLE images(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, listIndex INTEGER, imageType INTEGER, imageLocation TEXT, ownerId INTEGER NOT NULL, CONSTRAINT fk_recipes FOREIGN KEY(ownerId) REFERENCES recipes(id))');
+				await db.execute('CREATE TABLE images(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, listIndex INTEGER, imageType INTEGER, imageLocation TEXT, ownerId INTEGER NOT NULL)');
 				await db.execute('CREATE TABLE tags(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT)');
 				await db.execute('CREATE TABLE mn_recipes_tags(recipeId INTEGER NOT NULL, tagId INTEGER NOT NULL)');
 			},
+			/*
 			onConfigure: (db) async {
 				await db.execute('PRAGMA foreign_keys = ON');
 			},
+			*/
 			version: 1,
 		);
 	}
