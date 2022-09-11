7091ad13ccbb2cde4f8ec7232819b63631dc9bc9
MapleJupiter <sunflower-beacon@protonmail.com>
2022-09-11 21:31:22 +0800

    Implement the first version of a landscape view, for Search Screen.

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -9,8 +9,49 @@ import '../widgets/main_drawer.dart';
 class SearchScreen extends StatelessWidget {
 	static const routeName = '/search';
 
+	Widget _componentsList(Orientation orientation, BoxConstraints constraints) {
+		const componentsList = <Widget>[
+			SingleChildScrollView(
+				child: SearchForm()
+			),
+			Expanded(
+				child: SearchResult(),
+			),
+		];
+
+		if (orientation == Orientation.portrait) {
+			return Column(
+				children: const <Widget>[
+					SingleChildScrollView(
+						child: SearchForm()
+					),
+					Expanded(
+						child: SearchResult(),
+					),
+				],
+			);
+		}
+
+		return Row(
+			children: <Widget>[
+				Container(
+					height: constraints.maxHeight,
+					width: constraints.maxWidth * 0.55,
+					child: const SingleChildScrollView(
+						child: SearchForm()
+					),
+				),
+				const Expanded(
+					child: SearchResult(),
+				),
+			],
+		);
+	}
+
 	@override
 	Widget build(BuildContext context) {
+		final orientation = MediaQuery.of(context).orientation;
+
 		return Scaffold(
 			appBar: AppBar(
 				title: const Text('Search')
@@ -18,16 +59,9 @@ class SearchScreen extends StatelessWidget {
 			drawer: MainDrawer(),
 			body: ChangeNotifierProvider(
 				create: (context) => RecipeSearchProvider(),
-				child: Column(
-					children: const <Widget>[
-						SingleChildScrollView(
-							child: SearchForm()
-						),
-						Expanded(
-							child: SearchResult(),
-						),
-					],
-				),
+				child: LayoutBuilder(builder: (ctx, constraints) {
+					return _componentsList(orientation, constraints);
+				}),
 			),
 		);
 	}
